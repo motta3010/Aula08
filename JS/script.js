137683355b167856a0b6a2e84acd68a2350c19a1
motta3010 <163685698+motta3010@users.noreply.github.com>
2024-10-31 21:05:06 -0300

    atualização

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+function verde(){
+    document.getElementById("corpo").style.backgroundColor="#0f0";
+}
+
+function ama(){
+    document.getElementById("corpo").style.backgroundColor="#ff0";
+}
+
+function azu(){
+    document.getElementById("corpo").style.backgroundColor="#00f";
+}
+
+function nome(){
+    if(document.getElementById("cxnome").value==""){
+        alert("digite um valor, campo em branco")
+    }
+    else{
+            alert("gravado com sucesso")
+        }
+    }
+
+    function nome(){
+        if(document.getElementById("cxemail").value==""){
+            alert("digite um valor, campo em branco")
+        }
+        else{
+                alert("gravado com sucesso")
+            }
+        }
